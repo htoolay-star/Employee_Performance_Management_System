adda74f2bebfe4d60f3a0e0ecda64c14e22879f0
Htoo <htoopyaesone8834@gmail.com>
2026-04-23 22:50:37 +0630

    add fix some

--- EPMS.Domain/Data/Configurations/Performance/AppraisalDetailConfiguration.cs
+++ EPMS.Domain/Data/Configurations/Performance/AppraisalDetailConfiguration.cs
@@ -22,9 +22,9 @@ namespace EPMS.Domain.Data.Configurations.Performance
             entity.Property(e => e.ActualValue).HasMaxLength(100);
             entity.Property(e => e.Remarks).HasMaxLength(500);
 
-            entity.Property(e => e.Weightage).IsRequired();
-            entity.Property(e => e.Score).IsRequired();
-            entity.Property(e => e.WeightedScore).IsRequired();
+            entity.Property(e => e.Weightage).HasColumnType("decimal(5,2)").IsRequired();
+            entity.Property(e => e.Score).HasColumnType("decimal(5,2)").IsRequired();
+            entity.Property(e => e.WeightedScore).HasColumnType("decimal(5,2)").IsRequired();
 
             entity.HasOne(e => e.Appraisal)
                   .WithMany(a => a.Details)
