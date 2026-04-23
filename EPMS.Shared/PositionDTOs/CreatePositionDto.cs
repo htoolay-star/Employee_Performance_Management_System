b73eec303c22ba7681da54334c3fe90732e06fa0
Tun Thant Zaw <htunthantzaw2017@gmail.com>
2026-04-23 15:25:53 +0630

    positiondtos

---
+++ EPMS.Shared/PositionDTOs/CreatePositionDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPMS.Shared.PositionDTOs
+{
+    public class CreatePositionDto
+    {
+        [Required(ErrorMessage = "Position name is required")]
+        public string Position_Name { get; set; } = "";
+
+        [Required]
+        public int Level_ID { get; set; }
+
+        [Required]
+        public int Department_ID { get; set; }
+
+        public int? Team_ID { get; set; }
+    }
+}
