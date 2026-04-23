6f4f7077e9d728b59ec64b1493feba0341434c5d
Tun Thant Zaw <htunthantzaw2017@gmail.com>
2026-04-23 15:26:33 +0630

    positionRespnseDtos

---
+++ EPMS.Shared/PositionDTOs/PositionResponseDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPMS.Shared.PositionDTOs
+{
+    public class PositionResponseDto
+    {
+        public int Position_ID { get; set; }
+        public string Position_Name { get; set; } = "";
+        public int Level_ID { get; set; }
+        public int Department_ID { get; set; }
+        public int? Team_ID { get; set; }
+        public string Level_Code { get; set; } = "";
+        public string Level_Name { get; set; } = "";
+        public string Department_Name { get; set; } = "";
+        public string? Team_Name { get; set; }
+        public bool Is_Active { get; set; }
+    }
+}
---
+++ EPMS.Shared/PositionDTOs/UpdatePositionDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPMS.Shared.PositionDTOs
+{
+    internal class UpdatePositionDto : CreatePositionDto
+    {
+        public bool Is_Active { get; set; } = true;
+    }
+    
+}
