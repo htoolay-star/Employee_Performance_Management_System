fa94a3d2f9330285f0da82c7d82b5d909ba5cbf7
Htoo <htoopyaesone8834@gmail.com>
2026-04-26 23:35:26 +0630

    Add FluentValidation and centralized auth validation messages
    Added AuthValidationMessages for reusable validation messages.
    Implemented validators for auth DTOs using FluentValidation.
    Introduced ValidationExtensions for common email/password rules.
    Updated project to include FluentValidation DI package.

---
+++ EPMS.Shared/Validators/AuthValidators/AdminResetPasswordRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using EPMS.Shared.Constants.ValidationMessages;
+using EPMS.Shared.DTOs.Auth;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPMS.Shared.Validators.AuthValidators
+{
+    public class AdminResetPasswordRequestValidator : AbstractValidator<AdminResetPasswordRequest>
+    {
+        public AdminResetPasswordRequestValidator()
+        {
+            RuleFor(x => x.NewPassword).ApplyPasswordRules();
+
+            RuleFor(x => x.ConfirmPassword)
+                .NotEmpty().WithMessage(AuthValidationMessages.Password.ConfirmRequired)
+                    .Equal(x => x.NewPassword).WithMessage(AuthValidationMessages.Password.Mismatch);
+        }
+    }
+}
