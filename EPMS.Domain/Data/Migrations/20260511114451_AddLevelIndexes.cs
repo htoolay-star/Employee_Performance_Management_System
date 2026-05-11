3e8e55853bb9e6ee4a46cbf2b6907470495258fa
Htoo <htoopyaesone8834@gmail.com>
2026-05-11 18:21:26 +0630

    fix level entity index

---
+++ EPMS.Domain/Data/Migrations/20260511114451_AddLevelIndexes.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPMS.Domain.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLevelIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Levels_IsActive",
+                schema: "hr",
+                table: "Levels",
+                column: "IsActive",
+                filter: "[IsActive] = 1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Levels_Name",
+                schema: "hr",
+                table: "Levels",
+                column: "Name",
+                unique: true,
+                filter: "[IsDeleted] = 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Levels_IsActive",
+                schema: "hr",
+                table: "Levels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Levels_Name",
+                schema: "hr",
+                table: "Levels");
+        }
+    }
+}
